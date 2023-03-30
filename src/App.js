35afa74c60090f209db88db46f0332bbcb46064d
Bárbara Jovem <128049186+barbara-jovem@users.noreply.github.com>
2023-03-30 18:02:45 -0300

    Atualizações - Aula 02
    Jornada Dev EBAC - Exercício TikTok - Parte 02

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Video from "./pages/video";
+
+function App() {
+  return (
+    <div className="App">
+      <div className='app__videos'>
+      <Video />      
+    </div>
+    </div>
+  );
+}
+
+export default App;
